3f680f5e7a9bace6b56e8ea9ec7a3f8b2d8b920d
Douglas Fernandes <contato@douglasfernandes.com.br>
2020-03-07 20:13:52 -0300

    Hide youtube link

--- src/core/components/Navbar/index.jsx
+++ src/core/components/Navbar/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { ReactComponent as InstagramSVG } from "core/assets/imgs/instagram.svg";
 import { ReactComponent as GitHubSVG } from "core/assets/imgs/github.svg";
 import { ReactComponent as LinkedInSVG } from "core/assets/imgs/linkedin.svg";
-import { ReactComponent as YouTubeSVG } from "core/assets/imgs/youtube.svg";
+// import { ReactComponent as YouTubeSVG } from "core/assets/imgs/youtube.svg";
 import { ReactComponent as LogoSVG } from "core/assets/imgs/logo.svg";
 import Text from "core/components/Text";
 import Styled from "./styled";
@@ -33,12 +33,12 @@ const Navbar = ({ children }) => {
         >
           <LinkedInSVG />
         </Styled.Link>
-        <Styled.Link
+        {/* <Styled.Link
           href="https://www.youtube.com/channel/UCULnRixNG6dyQb9lo2xLrpg"
           target="_blank"
         >
           <YouTubeSVG />
-        </Styled.Link>
+        </Styled.Link> */}
       </Styled.Footer>
     </Styled.Wrapper>
   );
